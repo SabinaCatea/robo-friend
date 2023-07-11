6b96c705051edc574ea102952e04b9a5f1dfd583
Sabina Catea <cateasabina5@gmail.com>
2023-07-11 11:57:09 +0300

    upload the Robo Friend Project

---
+++ src/Robofriends/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// a searchbar where users can filter the robots displayed
+const SearchBar = ({ searchFunction }) => {
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Enter robot's name"
+        className="pa3 ba b--green bg-lightest-blue ma3"
+        onChange={searchFunction}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
